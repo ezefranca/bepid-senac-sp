9b03e92c29985f020f74e97092befe883e99b738
gabrielvieira <viera_frifri@hotmail.com>
2014-03-25 17:05:41 -0300

    projeto maneiro
    projeto maneiro

---
+++ Terceiro projeto/example/tempViewController.h
@@ -0,0 +1,19 @@
+//
+//  tempViewController.h
+//  RNFrostedSidebar
+//
+//  Created by GABRIEL VIEIRA on 25/03/14.
+//  Copyright (c) 2014 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RNFrostedSidebar.h"
+#import "PNChart.h"
+
+
+@interface tempViewController : UIViewController <RNFrostedSidebarDelegate,PNChartDelegate>
+
+- (IBAction)btn:(id)sender;
+@property (nonatomic, strong) NSMutableIndexSet *optionIndices;
+
+@end
---
+++ Terceiro projeto/example/tempViewController.m
@@ -0,0 +1,120 @@
+//
+//  tempViewController.m
+//  RNFrostedSidebar
+//
+//  Created by GABRIEL VIEIRA on 25/03/14.
+//  Copyright (c) 2014 Ryan Nystrom. All rights reserved.
+//
+
+#import "tempViewController.h"
+#import "ViewController.h"
+#import "PNChart.h"
+
+
+@interface tempViewController ()
+
+@end
+
+@implementation tempViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    UILabel * lineChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+    lineChartLabel.text = @"Temperatura";
+    lineChartLabel.textColor = PNFreshGreen;
+    lineChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+    lineChartLabel.textAlignment = NSTextAlignmentCenter;
+    
+    PNLineChart * lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 200.0)];
+    lineChart.backgroundColor = [UIColor clearColor];
+    [lineChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5",@"SEP 6",@"SEP 7"]];
+    
+    // Line Chart Nr.1
+    NSArray * data01Array = @[@60.1, @160.1, @126.4, @262.2, @186.2, @127.2, @176.2];
+    PNLineChartData *data01 = [PNLineChartData new];
+    data01.color = PNFreshGreen;
+    data01.itemCount = lineChart.xLabels.count;
+    data01.getData = ^(NSUInteger index) {
+        CGFloat yValue = [[data01Array objectAtIndex:index] floatValue];
+        return [PNLineChartDataItem dataItemWithY:yValue];
+    };
+    
+    // Line Chart Nr.2
+    NSArray * data02Array = @[@20.1, @180.1, @26.4, @202.2, @126.2, @167.2, @276.2];
+    PNLineChartData *data02 = [PNLineChartData new];
+    data02.color = PNTwitterColor;
+    data02.itemCount = lineChart.xLabels.count;
+    data02.getData = ^(NSUInteger index) {
+        CGFloat yValue = [[data02Array objectAtIndex:index] floatValue];
+        return [PNLineChartDataItem dataItemWithY:yValue];
+    };
+    
+    lineChart.chartData = @[data01, data02];
+    [lineChart strokeChart];
+    
+    lineChart.delegate = self;
+    
+    [self.view addSubview:lineChartLabel];
+    [self.view addSubview:lineChart];
+    
+    self.title = @"iMonitor Temperatura";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btn:(id)sender {
+           NSArray *images = @[
+                            [UIImage imageNamed:@"gear"],
+                            [UIImage imageNamed:@"globe"],
+                            [UIImage imageNamed:@"profile"],
+                            [UIImage imageNamed:@"star"],
+                            [UIImage imageNamed:@"gear"]];
+        
+        
+        NSArray *colors = @[
+                            [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+                            [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+                            [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+                            [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+                            [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1]];
+    
+        RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices borderColors:colors];
+        //    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images];
+        callout.delegate = self;
+        //    callout.showFromRight = YES;
+        [callout show];
+}
+
+
+- (void)sidebar:(RNFrostedSidebar *)sidebar didTapItemAtIndex:(NSUInteger)index {
+    NSLog(@"Tapped item at index %i",index);
+    
+    
+    if (index == 0) {
+        ViewController *t = [[ViewController alloc]initWithNibName:@"ViewController" bundle:nil];
+        
+        //SERA USADO O BLOCO
+        [self presentViewController:t animated:NO completion:nil];
+    }
+    /*
+     if (index == 3) {
+     [sidebar dismissAnimated:YES completion:nil];
+     }
+     */
+}
+@end
