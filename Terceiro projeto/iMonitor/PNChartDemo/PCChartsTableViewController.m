5fae29c5a646af6219c1039d3d127d3ac17d8cf7
Ezequiel França <ezequiel.ifsp@gmail.com>
2014-03-25 00:57:57 -0300

    Graficos (linhas, colunas e circular)

---
+++ Terceiro projeto/iMonitor/PNChartDemo/PCChartsTableViewController.m
@@ -0,0 +1,151 @@
+//
+//  PCChartsTableViewController.m
+//  PNChartDemo
+//
+//  Created by kevinzhow on 13-12-1.
+//  Copyright (c) 2013年 kevinzhow. All rights reserved.
+//
+
+#import "PCChartsTableViewController.h"
+#import "PNChart.h"
+#import "PNLineChartData.h"
+#import "PNLineChartDataItem.h"
+
+@interface PCChartsTableViewController ()
+
+@end
+
+@implementation PCChartsTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    
+    UIViewController * viewController = [segue destinationViewController];
+    
+    if ([segue.identifier isEqualToString:@"lineChart"]) {
+        
+        //Add LineChart
+        UILabel * lineChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+        lineChartLabel.text = @"Temperatura";
+        lineChartLabel.textColor = PNFreshGreen;
+        lineChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+        lineChartLabel.textAlignment = NSTextAlignmentCenter;
+        
+        PNLineChart * lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 200.0)];
+        lineChart.backgroundColor = [UIColor clearColor];
+        [lineChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5",@"SEP 6",@"SEP 7"]];
+
+        // Line Chart Nr.1
+        NSArray * data01Array = @[@60.1, @160.1, @126.4, @262.2, @186.2, @127.2, @176.2];
+        PNLineChartData *data01 = [PNLineChartData new];
+        data01.color = PNFreshGreen;
+        data01.itemCount = lineChart.xLabels.count;
+        data01.getData = ^(NSUInteger index) {
+            CGFloat yValue = [[data01Array objectAtIndex:index] floatValue];
+            return [PNLineChartDataItem dataItemWithY:yValue];
+        };
+
+        // Line Chart Nr.2
+        NSArray * data02Array = @[@20.1, @180.1, @26.4, @202.2, @126.2, @167.2, @276.2];
+        PNLineChartData *data02 = [PNLineChartData new];
+        data02.color = PNTwitterColor;
+        data02.itemCount = lineChart.xLabels.count;
+        data02.getData = ^(NSUInteger index) {
+            CGFloat yValue = [[data02Array objectAtIndex:index] floatValue];
+            return [PNLineChartDataItem dataItemWithY:yValue];
+        };
+
+        lineChart.chartData = @[data01, data02];
+        [lineChart strokeChart];
+        
+        lineChart.delegate = self;
+        
+        [viewController.view addSubview:lineChartLabel];
+        [viewController.view addSubview:lineChart];
+        
+        viewController.title = @"iMonitor Temperatura";
+        
+    }else if ([segue.identifier isEqualToString:@"barChart"])
+    {
+        //Add BarChart
+        
+        UILabel * barChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+        barChartLabel.text = @"Umidade";
+        barChartLabel.textColor = PNFreshGreen;
+        barChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+        barChartLabel.textAlignment = NSTextAlignmentCenter;
+        
+        PNBarChart * barChart = [[PNBarChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 200.0)];
+        barChart.backgroundColor = [UIColor clearColor];
+        [barChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5",@"SEP 6",@"SEP 7"]];
+        [barChart setYValues:@[@1,@24,@12,@18,@30,@10,@21]];
+        [barChart setStrokeColors:@[PNGreen,PNGreen,PNRed,PNGreen,PNGreen,PNYellow,PNGreen]];
+        [barChart strokeChart];
+        
+        [viewController.view addSubview:barChartLabel];
+        [viewController.view addSubview:barChart];
+        
+        viewController.title = @"iMonitor Umidade";
+    }else if ([segue.identifier isEqualToString:@"circleChart"])
+    {
+        
+        //Add CircleChart
+        
+        
+        UILabel * circleChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+        circleChartLabel.text = @"Ruido";
+        circleChartLabel.textColor = PNFreshGreen;
+        circleChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+        circleChartLabel.textAlignment = NSTextAlignmentCenter;
+        
+        PNCircleChart * circleChart = [[PNCircleChart alloc] initWithFrame:CGRectMake(0, 80.0, SCREEN_WIDTH, 100.0) andTotal:[NSNumber numberWithInt:100] andCurrent:[NSNumber numberWithInt:60] andClockwise:NO];
+        circleChart.backgroundColor = [UIColor clearColor];
+        [circleChart setStrokeColor:PNGreen];
+        [circleChart strokeChart];
+        
+        [viewController.view addSubview:circleChartLabel];
+        [viewController.view addSubview:circleChart];
+        viewController.title = @"iMonitor Ruido";
+        
+    }
+    
+}
+
+-(void)userClickedOnLineKeyPoint:(CGPoint)point lineIndex:(NSInteger)lineIndex andPointIndex:(NSInteger)pointIndex{
+    NSLog(@"Click Key on line %f, %f line index is %d and point index is %d",point.x, point.y,(int)lineIndex, (int)pointIndex);
+}
+
+-(void)userClickedOnLinePoint:(CGPoint)point lineIndex:(NSInteger)lineIndex{
+    NSLog(@"Click on line %f, %f, line index is %d",point.x, point.y, (int)lineIndex);
+}
+
+@end
