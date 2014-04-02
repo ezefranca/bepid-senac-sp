03c7801d03c728366263b2fecac4165a793ea8b7
gabrielvieira <viera_frifri@hotmail.com>
2014-04-02 16:57:13 -0300

    classe de animacao
    classe de animacao

---
+++ Terceiro projeto/Class/Models/Simutation/Simulation.h
@@ -0,0 +1,15 @@
+//
+//  Simulation.h
+//  RNFrostedSidebar
+//
+//  Created by GABRIEL VIEIRA on 02/04/14.
+//  Copyright (c) 2014 Ryan Nystrom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Simulation : NSObject
+
+-(void)start : (id) view;
+
+@end
---
+++ Terceiro projeto/Class/Models/Simutation/Simulation.m
@@ -0,0 +1,63 @@
+//
+//  Simulation.m
+//  RNFrostedSidebar
+//
+//  Created by GABRIEL VIEIRA on 02/04/14.
+//  Copyright (c) 2014 Ryan Nystrom. All rights reserved.
+//
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
+#import "Simulation.h"
+
+@implementation Simulation
+
+-(void)start : (UIView *) view;
+{
+
+    NSLog(@"%f   %f", view.bounds.size.height ,view.bounds.size.width );
+    
+    //OBJETOS
+
+    //quadrado
+    UIView *redSquare = [[UIView alloc]initWithFrame:CGRectMake(20, 20, 50, 50)];
+    redSquare.backgroundColor = [UIColor redColor];
+    [redSquare setTransform:CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(20))];
+    [view addSubview:redSquare];
+    
+    
+    //linha
+    UIView *line = [[UIView alloc]initWithFrame:CGRectMake(0, 300, 320, 20)];
+    line.backgroundColor = [UIColor yellowColor];
+    // [self.line setTransform:CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(20))];
+    [view addSubview:line];
+    
+    // INICIAR ANIMACAO
+    UIDynamicAnimator *animator = [[UIDynamicAnimator alloc] initWithReferenceView:view];
+    
+    //CUSTON
+    
+    //quadrado
+    UIDynamicItemBehavior *elasticityBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[redSquare]];
+    elasticityBehavior.elasticity = 0.0f;
+    [elasticityBehavior setDensity:0.0];
+    [elasticityBehavior setElasticity:1.0   ];
+    [animator addBehavior:elasticityBehavior];
+    
+    //linha
+    UIDynamicItemBehavior *teste = [[UIDynamicItemBehavior alloc] initWithItems:@[line]];
+    [teste setDensity:50000000.0];
+    [teste setFriction:0.3];
+    [animator addBehavior:teste];
+    
+    
+    //COLISAO E GRAEVIDADE
+    UIGravityBehavior* gravityBehavior = [[UIGravityBehavior alloc] initWithItems:@[redSquare ]];
+    [animator addBehavior:gravityBehavior];
+    
+    UICollisionBehavior* collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[redSquare , line]];
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    [animator addBehavior:collisionBehavior];
+
+}
+
+@end
